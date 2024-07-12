a6cf8e1a84b60bfce90b23f9161c60d602777d94
Zuriel <chiekezuriel2020@gmail.com>
2024-07-12 16:14:00 +0100

    first react commit

---
+++ src/app/dashboard/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const dashboard = () => {
+  return (
+    <div>
+      fashhh
+    </div>
+  )
+}
+
+export default dashboard
---
+++ src/app/page.js
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import Loading from "@/components/Loading";
+
+export default function Home() {
+  return (
+    <div>
+    Home
+    <Loading/>
+    </div>
+  );
+}
